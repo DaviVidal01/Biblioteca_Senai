c8ac5fd6cc10b6540bf4b2ee49cf9d3aad253397
DaviVidal01 <davividal0605@gmail.com>
2023-09-19 20:46:16 -0300

    Configurando o Django e o MySQL

--- Biblioteca_senai - Principal/X-Books/Website/models.py
+++ Biblioteca_senai - Principal/X-Books/Website/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+NEWES = (
+    ('1', 'None'),
+    ('2', 'New')
+)
+DESC = (
+    ('1', 'None'),
+    ('2', '-10%'),
+    ('3', '-30%'),
+)
+
+class NewProdutos(models.Model):
+    title = models.CharField(default='', max_length= 30)
+    image = models.ImageField(upload_to= './images')
+    precoD = models.DecimalField(default=000.00, max_digits=5, decimal_places=2)
+    precoA = models.DecimalField(default=000.00, max_digits=5, decimal_places=2)
+    new = models.CharField(max_length = 20, choices = NEWES, default = '1')
+    desconto = models.CharField(max_length = 20, choices = DESC, default = '1')
+
+class Categorias(models.Model):
+    nome = models.CharField(default='', max_length= 30)
