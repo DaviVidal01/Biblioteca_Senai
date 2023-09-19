f8541167fe3391923dc68f16e6132f1bc90c9f00
DaviVidal01 <davividal0605@gmail.com>
2023-09-18 21:55:18 -0300

    Configurando o Django e as Urls de cada página.

--- Biblioteca_senai - Principal/X-Books/Website/views.py
+++ Biblioteca_senai - Principal/X-Books/Website/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request,"index.html")
+    
+def livros(request):
+    return render(request,"livros.html")
+
+def catalogo(request):
+    return render(request,"catalogo.html")
+
+def catalogo2(request):
+    return render(request,"catalogo-2.html")
+
+def FAQs(request):
+    return render(request,"FAQs.html")
+
+def admin(request):
+    return render(request,"admin.html")
+
+def descricao(request):
+    return render(request,"descricao.html")
--- Biblioteca_senai - Principal/X-Books/setup/settings.py
+++ Biblioteca_senai - Principal/X-Books/setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Website',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +56,9 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'Website/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,8 +118,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
