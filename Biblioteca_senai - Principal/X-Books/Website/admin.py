18265701c4e65ef062bfcac358e9e78e10ce8ca8
DaviVidal01 <davividal0605@gmail.com>
2023-09-20 19:29:31 -0300

    Finalizando a criação dos bancos Livros, Usuários e Registro.

--- Biblioteca_senai - Principal/X-Books/Website/admin.py
+++ Biblioteca_senai - Principal/X-Books/Website/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(NewProdutos)
+admin.site.register(Registro)
+admin.site.register(Usuario)
+admin.site.register(Livros)
--- Biblioteca_senai - Principal/X-Books/Website/models.py
+++ Biblioteca_senai - Principal/X-Books/Website/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-from django.utils import datetime
+from datetime import datetime
+
 # Create your models here.
+CATEGORIAS = (
+    ('1','Aventura'),
+    ('2','Terror'),
+    ('3','Romance'),
+    ('4','Poesia'),
+    ('5','Fantasia'),
+    ('6','Biografia'),
+    ('7','Autobiografia'),
+)
 STATUS = (
     ('1', 'Disponível'),
     ('2', 'Alugado')
@@ -20,22 +30,19 @@ class Livros(models.Model):
     titulo = models.CharField(default='', max_length= 30)
     autor = models.CharField(default='', max_length= 30)
     image = models.ImageField(upload_to= './images')
-    genero = models.CharField(max_length= 20, choices=,  default ='1') 
+    genero = models.CharField(max_length= 20, choices=CATEGORIAS,  default ='1') 
     status = models.CharField(max_length = 20, choices = STATUS, default = '1')
 
 class Usuario(models.Model):
     titulo = models.CharField(default='', max_length= 30)
     email = models.EmailField(default='', max_length= 40)
     cpf = models.CharField(default='', max_length= 11)
-    telefone = models.IntegerField(default='', max_digits= 14)
+    telefone = models.CharField(default='4002-8922', max_length=14)
     senha = models.CharField(default='', max_length= 40)
     endereco = models.TextField(default='', max_length=255)
     ficha = models.CharField(max_length= 20, choices= FICHA, default='1')
 
-class Categorias(models.Model):
-    titulo = models.CharField(default='', max_length= 30)
-
 class Registro(models.Model):
-    data_Ret = models.DateTimeField(default=datetime.datetime.now())
-    data_Dev = models.DateTimeField(default=datetime.datetime.now())
+    data_Ret = models.DateTimeField(default=datetime.now())
+    data_Dev = models.DateTimeField(default=datetime.now())
     status = models.CharField(max_length=20, choices= STATUS2, default= '1')
