0052c23356dab2a28c416236da033a3451c39498
DaviVidal01 <davividal0605@gmail.com>
2023-10-20 22:25:17 -0300

    Removendo códigos desnecessários e organizando

--- Biblioteca_senai - Principal/X-Books/Website/urls.py
+++ Biblioteca_senai - Principal/X-Books/Website/urls.py
@@ -2,27 +2,29 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
+# -------- Pages Guests
     path("", views.index, name="index"),
     path('livros/', views.livros, name="livros"),
     path('catalogo/', views.catalogo, name="catalogo"),
-    path('catalogo2/', views.catalogo2, name="catalogo2"),
-    path('funcionario/', views.funcionario, name="funcionario"),
-    path('funcionario1/', views.funcionario1, name="funcionario1"),
-    path('funcionario2/', views.funcionario2, name="funcionario2"),
-    path('funcionario3/', views.funcionario3, name="funcionario3"),
     path('FAQs/', views.FAQs, name="FAQs"),
-    path('descricao/', views.descricao, name="descricao"),
-    path('cadastro/', views.cadastro, name="cadastro"),
-    path('cadastro_user/', views.cadastro_usuarios, name="cadastro"),
-    path('cadastro_book/', views.cadastro_book, name="cadastro_book"),
-    path('consulta_book/', views.consulta_book, name="cadastro"),
-    path('consulta_user/', views.consulta_user, name="cadastro"),
-    path('atualiza_user/', views.atualiza_user, name="atualizacao"),
-    path('atualiza_book/', views.atualiza_book, name="atualizacao"),
     path('login/', views.login, name="login"),
     path('feedback/', views.feedback, name="feedback"),
-    path('cadastrarL', views.cadastrarL, name="cadastrarL"),
+
+# -------- Pages Admin
+    path('funcionario/', views.funcionario, name="funcionario"),
+    path('cadastro/', views.cadastro, name="cadastro"),
+
+# -------- Admin User
     path('cadastrarU/', views.cadastrarU, name="cadastrarU"),
+    path('cadastro_user/', views.cadastro_usuarios, name="cadastro"),
+    path('consulta_user/', views.consulta_user, name="cadastro"),
+    path('atualiza_user/', views.atualiza_user, name="atualizacao"),
+
+# -------- Admin Book
+    path('cadastro_book/', views.cadastro_book, name="cadastro_book"),
+    path('consulta_book/', views.consulta_book, name="cadastro"),
+    path('atualiza_book/', views.atualiza_book, name="atualizacao"),
+    path('cadastrarL', views.cadastrarL, name="cadastrarL"),
     path('reserva_livros/', views.status, name="reserva_livros"),
 
 # -------- Auth Login
